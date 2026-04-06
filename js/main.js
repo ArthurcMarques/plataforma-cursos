b01a7094645c8e3cb2be76a664de0b2a98ad2e47
ArthurMarques <arthurmcardoso11@gmail.com>
2026-04-05 22:19:53 -0300

    fix(main): garantir data padrao antes de carregar controllers

--- js/main.js
+++ js/main.js
@@ -29,10 +29,21 @@ document.addEventListener("DOMContentLoaded", () => {
     aplicarDataPadraoHoje();
 });
 
-import "./controllers/categoriasController.js";
-import "./controllers/usuariosController.js";
-import "./controllers/cursosController.js";
-import "./controllers/modulosAulasController.js";
-import "./controllers/matriculasProgressoController.js";
-import "./controllers/planosAssinaturasPagamentosController.js";
-import "./controllers/trilhasController.js";
+function carregarControllers() {
+    const controllers = [
+        "./controllers/categoriasController.js",
+        "./controllers/usuariosController.js",
+        "./controllers/cursosController.js",
+        "./controllers/modulosAulasController.js",
+        "./controllers/matriculasProgressoController.js",
+        "./controllers/planosAssinaturasPagamentosController.js",
+        "./controllers/trilhasController.js"
+    ];
+
+    controllers.forEach((caminho) => {
+        import(caminho);
+    });
+}
+
+carregarControllers();
+
